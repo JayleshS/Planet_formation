5945efcdcef4e07bd189a9ba12545077b2a2c410
jellemeijn <jellemeijn@hotmail.com>
2018-02-14 15:59:22 +0100

    test

--- pars.py
+++ pars.py
@@ -5,6 +5,6 @@ mSun= 1.9884754153381438e+33 #mass of the Sun (in grams)
 mEarth = 5.9722e27 #mass of Earth (in grams)
 au = 1.495978707e13 #astronomical unit (in cm)
 yr = 2*np.pi /np.sqrt(gN*mSun/au**3) #1 year in seconds
-conflict= 2.
+conflict= 53532.
 #problem parameters
 Np = 2 #2 particles
