1739cf514659904caeef703aed82cd942fe6ff43
jellemeijn <jellemeijn@hotmail.com>
2018-02-21 16:46:47 +0100

    hermite toegevoegd, nog niet getest

--- 2body_matrix_form.py
+++ 2body_matrix_form.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 # xarr, varr, marr = fn.init_2body(0)
 # etot0 = fn.e_tot(xarr, varr, marr)
 
-tfinal = 2.5*pars.yr
-dt = 0.001*pars.yr
+tfinal = 1e8
+dt = 0.01*pars.yr
 # etot0 = fn.e_tot(xarr, varr, marr)
 
 x_and_v =[]
@@ -38,22 +38,19 @@ def midpoint(dt, tfinal):
 	while time < tfinal:
 		acc = fn.forces(particles, marr)
 		particles_mid = np.zeros((pars.Np, 2, 3))
-		particles_mid[:, 0, :] = particles[:,0,:] + particles[:,1,:] * dt / 2
-		particles_mid[:, 1, :] = particles[:,1,:] + acc * dt / 2
+		particles_mid[:, 0, :] = particles[:, 0, :] + particles[:, 1, :] * dt / 2
+		particles_mid[:, 1, :] = particles[:, 1, :] + acc * dt / 2
 
 		amid = fn.forces(particles_mid, marr)
 		particles[:, 0, :] += particles_mid[:, 1, :] * dt
-		particles[:,1,:] += amid * dt
+		particles[:, 1, :] += amid * dt
 
 		x_and_v.append(particles.tolist())
-
 		time += dt
 
 	etot1 = fn.e_tot(particles, marr)
 	e_error = (etot1 - etot0) / etot0
-	print 'dag'
-
-	return particles, e_error
+	return x_and_v, e_error
 
 def leapfrog(dt, tfinal):
 	particles, marr = fn.init_2body(0)
@@ -72,6 +69,30 @@ def leapfrog(dt, tfinal):
 	e_error = (etot1 - etot0) / etot0
 
 	return particles
+
+
+def hermite(dt, tfinal):
+	particles, marr = fn.init_2body(0)
+	etot0 = fn.e_tot(particles, marr)
+	time = 0
+	iterations = 2
+
+	while time < tfinal:
+		acc, jerk = fn.forces_hermite(particles, marr)
+
+		old_x = np.copy(particles[:, 0, :])
+		old_v = np.copy(particles[:, 1, :])
+		old_acc = np.copy(acc)
+		old_jerk = np.copy(jerk)
+
+		particles[:, 0, :] += particles[:, 1, :] + acc * dt**2 / 2 + jerk * dt**3 / 6
+		particles[:, 1, :] += acc * dt + jerk * dt**2 / 2
+		for i in range(iterations):
+			varr = old_v + (old_a + acc)*dt/2 + ((old_j - jerk)*dt**2)/12
+			xarr = old_x + (old_v + particles[:, 1, :])*dt/2 + ((alpha/10)*((old_a - acc)*dt**2)) + ((6*alpha - 5)/120)*(old_j + jerk)*dt**3
+
+	pass
+
 
 def plot(x1_val, y1_val, x2_val, y2_val):
 	# plt.plot(x1_val, y1_val)
@@ -96,9 +117,11 @@ def plottest(particles):
 
 
 def main():
-	# print dt
-	pos_euler, error_euler = euler(dt, tfinal)
-	plottest(pos_euler)
+	pos_mid, error_mid = midpoint(dt, tfinal)
+	# pos_euler, error_euler = euler(dt, tfinal)
+	# plottest(pos_euler)
+	plottest(pos_mid)
+	print error_mid
 	# print xv
 
 if __name__ == '__main__':
--- functions_matrix_form.py
+++ functions_matrix_form.py
@@ -88,14 +88,38 @@ def forces(particles, marr):
     acc = np.zeros((pars.Np,3))
     for i in range(pars.Np):
         for j in range(i+1, pars.Np):
-            rji = particles[j,0,:] - particles[i,0,:]
+            rji = particles[j, 0, :] - particles[i, 0, :]
 
-            # acc[i,:] += pars.gN*rji/sum(rji**2)**(3/2) * marr[j]
-            # acc[j,:] -= pars.gN*rji/sum(rji**2)**(3/2) * marr[i]
-            acc[i, :] += pars.gN*rji/(np.sqrt(sum(rji**2))*sum(rji**2))*marr[j]
-            acc[j, :] -= pars.gN*rji/(np.sqrt(sum(rji**2))*sum(rji**2))*marr[i]
+            # acc[i,:] += pars.gN*rji/(sum(rji**2)**(3/2)) * marr[j]
+            # acc[j,:] -= pars.gN*rji/(sum(rji**2)**(3/2)) * marr[i]
+            acc[i, :] += pars.gN * rji / (np.sqrt(sum(rji**2)) * sum(rji**2)) * marr[j]
+            acc[j, :] -= pars.gN * rji / (np.sqrt(sum(rji**2)) * sum(rji**2)) * marr[i]
 
     return acc
+
+
+def forces_hermite(particles, marr):
+    acc = np.zeros((pars.Np, 3))
+    jer = np.zeros((pars.Np, 3))
+    for i in range(pars.Np):
+        for j in range(pars.Np):
+            rji = particles[j, 0, :] - particles[i, 0, :]
+            vji = particles[j, 1, :] - particles[i, 1, :]
+
+            r2 = sum(rji**2)
+            r1 = np.sqrt(r2)
+            r3 = r1 * r2
+            rv = sum(rji * vji)
+            rv /= r2
+
+            acc[i, :] += pars.gN * (rji / r3) * marr[j]
+            acc[j, :] -= pars.gN * (rji / r3) * marr[i]
+
+            jerk = (vji - 3 * rv * rji) / r3
+            jer[i, :] += pars.gN * jerk * marr[j]
+            jer[i, :] -= pars.gN * jerk * marr[i]
+
+    return acc, jer
 
 def e_tot(particles, marr):
     Ekin = 0
